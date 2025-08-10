d204cd03b76363f4f0a7b5594fdc679dc05541a9
Hao Huang <huang.6224@osu.edu>
2025-08-09 20:46:23 -0400

    Fix syntax spacing

--- src/wpilib_cli/prompts.py
+++ src/wpilib_cli/prompts.py
@@ -7,6 +7,7 @@ def ask_project_name():
         "📛 Enter your project name:"
     ).ask()
 
+
 def ask_team_number():
     """Prompts the user for their FRC team number"""
     while True:
@@ -20,6 +21,7 @@ def ask_team_number():
         if confirm:
             return team_num
 
+
 def ask_team_domain(team_number):
     """Prompts the user for their team domain, with a default based on the team number"""
     has_domain = questionary.confirm("🌐 Does your team have a domain?", default=False).ask()
@@ -30,12 +32,14 @@ def ask_team_domain(team_number):
     else:
         return f"frc.team{team_number}.org"
 
+
 def select_wpilib_version(versions):
     """Prompts the user to select a WPILib version"""
     return questionary.select(
         "🔧 Select WPILib version:",
         choices=versions
     ).ask()
+
 
 def select_project_type():
     """Prompts the user to select the type of project to start with"""
@@ -47,6 +51,7 @@ def select_project_type():
         ]
     ).ask()
 
+
 def select_template(templates):
     """Prompts the user to select a project template"""
     choices = [
@@ -57,10 +62,12 @@ def select_template(templates):
         for template in templates
     ]
 
+
     return questionary.select(
         "📦 Select a project template:",
         choices=choices
     ).ask()
+
 
 def select_example(examples):
     """Prompts the user to select a project example"""
@@ -75,6 +82,7 @@ def select_example(examples):
         "📦 Select a project example: (Use arrow keys to select | Press enter to confirm)",
         choices=choices
     ).ask()
+
 
 def ask_programming_language():
     """Prompts the user to select a programming language for the project"""
