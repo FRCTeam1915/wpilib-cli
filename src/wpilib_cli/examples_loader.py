f6066d09fe9f75b675ff259534710bc31a8582ff
Hao Huang <huang.6224@osu.edu>
2025-08-09 20:45:52 -0400

    Change the func name from `load` to `fetch`

--- src/wpilib_cli/examples_loader.py
+++ src/wpilib_cli/examples_loader.py
@@ -4,7 +4,7 @@ import requests
 URL = ("https://raw.githubusercontent.com/wpilibsuite/allwpilib/refs/heads/main/wpilibjExamples/src/main/java/edu/wpi"
        "/first/wpilibj/examples/examples.json")
 
-def load_examples_from_github():
+def fetch_examples_from_github():
     """Fetches WPILib examples from GitHub"""
     res = requests.get(URL)
     res.raise_for_status()
