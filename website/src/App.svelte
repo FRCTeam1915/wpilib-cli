0da6af748169576e60d9c05bd1fca29a12e02ad9
Hao Huang <huang.6224@osu.edu>
2025-08-13 00:31:36 -0400

    Add the docs website source

---
+++ website/src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Working In Progress</h1>
+
+  <p>
+    Check out <a href="https://github.com/FRCTeam1915/wpilib-cli#readme" target="_blank" rel="noreferrer">wpilib-cli</a>!
+  </p>
+
+  <p class="copyright">
+      Copyright (c) 2025 Hao Huang (huang.6224@osu.edu), Adrian Johnson (drinz@mit.edu), Dane Briscoe (dbriscoe@highpoint.edu)
+  </p>
+</main>
+
+<style>
+  .copyright {
+    color: #888;
+  }
+</style>
